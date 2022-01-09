4cb05f2ed639e85cc3b91ca01779137a62f656b8
Mark Fisher <mark.j.fisher@gmail.com>
2022-01-09 11:45:05 +0000

    2015 day 4 - advent coins

--- aoc2015/src/lib.rs
+++ aoc2015/src/lib.rs
@@ -3,9 +3,11 @@ extern crate itertools;
 
 #[macro_use]
 extern crate aoc_runner_derive;
+extern crate crypto;
 
 pub mod day_01;
 pub mod day_02;
 pub mod day_03;
+pub mod day_04;
 
 aoc_lib! { year = 2015 }
