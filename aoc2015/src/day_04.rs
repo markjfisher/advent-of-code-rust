ad1ff1c69f01a387adf800ca380515e134bb10a6
Mark Fisher <mark.j.fisher@gmail.com>
2022-01-09 13:04:52 +0000

    refactor tests to run from near solution so they run faster

--- aoc2015/src/day_04.rs
+++ aoc2015/src/day_04.rs
@@ -3,14 +3,23 @@ use crypto::md5::Md5;
 
 #[aoc(day4, part1)]
 pub fn part1(secret: &str) -> u32 {
-    solver(secret, 0, |hash| hash[0..2] == [0; 2] && (hash[2] & 0xF0) == 0)
+    part1_solve(secret, 0)
+}
+
+fn part1_solve(secret: &str, start_at: u32) -> u32 {
+    solver(secret, start_at, |hash| hash[0..2] == [0; 2] && (hash[2] & 0xF0) == 0)
 }
 
 #[aoc(day4, part2)]
 pub fn part2(secret: &str) -> u32 {
     // use the value from part1 as starting position to reduce time a bit. from 140ms to 105ms in release
-    solver(secret, 254575, |hash| hash[0..3] == [0; 3])
+    part2_solve(secret, 254575)
 }
+
+fn part2_solve(secret: &str, start_at: u32) -> u32 {
+    solver(secret, start_at, |hash| hash[0..3] == [0; 3])
+}
+
 
 fn solver(secret: &str, start_at: u32, is_valid: impl Fn(&[u8; 16]) -> bool) -> u32 {
     let mut hash = [0; 16];
@@ -36,7 +45,7 @@ mod tests {
     // If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043
     // starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
     fn example1() {
-        assert_eq!(part1("abcdef"), 609043);
+        assert_eq!(part1_solve("abcdef", 609000), 609043);
     }
 
     #[test]
@@ -44,6 +53,6 @@ mod tests {
     // starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like
     // 000006136ef....
     fn example2() {
-        assert_eq!(part1("pqrstuv"), 1048970);
+        assert_eq!(part1_solve("pqrstuv", 1048900), 1048970);
     }
 }
