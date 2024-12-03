58bce1ebe4ebb1a41fb3e0e0d61567e8dc0978f3
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-03 07:06:59 +0000

    2024 day03 part 1

--- src/aoc2024/day03.rs
+++ src/aoc2024/day03.rs
@@ -1,12 +1,23 @@
-
-pub fn parse(_input: &str) -> Vec<u32> {
-    vec![]
+pub fn parse(input: &str) -> Vec<(u32, u32)> {
+    extract_muls(input)
 }
 
-pub fn part1(_input: &[u32]) -> u32 {
-    123
+pub fn part1(input: &[(u32, u32)]) -> u32 {
+    input.iter().map(|(x, y)| x * y).sum()
 }
 
-pub fn part2(_input: &[u32]) -> u32 {
+pub fn part2(_input: &[(u32, u32)]) -> u32 {
     456
+}
+
+pub fn extract_muls(s: &str) -> Vec<(u32, u32)> {
+    let mut result = Vec::new();
+    let re = regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    
+    for cap in re.captures_iter(s) {
+        if let (Ok(x), Ok(y)) = (cap[1].parse::<u32>(), cap[2].parse::<u32>()) {
+            result.push((x, y));
+        }
+    }
+    result
 }
