e38c356d5ffefc70f66a7e3d9f03b452d103a9d7
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-06 12:19:04 +0000

    Only store and check turning points. 350ms -> 34ms

--- src/aoc2024/day06.rs
+++ src/aoc2024/day06.rs
@@ -1,4 +1,4 @@
-use crate::util::{grid::*, point::*, hash::*};
+use crate::util::{grid::*, hash::*, point::*};
 
 pub fn parse(input: &str) -> Grid<u8> {
     Grid::parse(input)
@@ -9,9 +9,10 @@ fn walk_grid(input: &Grid<u8>) -> Option<FastSet<Point>> {
     let mut positions = FastSet::with_capacity((input.width * input.height) as usize);
     let mut guard_location = input.find(b'^').unwrap();
     let mut guard_direction = UP;
-    
+
     positions.insert(guard_location);
-    visited[(guard_location.y * input.width + guard_location.x) as usize][guard_direction.to_index()] = true;
+    visited[(guard_location.y * input.width + guard_location.x) as usize]
+        [guard_direction.to_index()] = true;
 
     loop {
         let new_location = guard_location + Point::from(guard_direction);
@@ -25,11 +26,14 @@ fn walk_grid(input: &Grid<u8>) -> Option<FastSet<Point>> {
             guard_location = new_location;
             positions.insert(guard_location);
         }
-        
-        if visited[(guard_location.y * input.width + guard_location.x) as usize][guard_direction.to_index()] {
+
+        if visited[(guard_location.y * input.width + guard_location.x) as usize]
+            [guard_direction.to_index()]
+        {
             return None;
         }
-        visited[(guard_location.y * input.width + guard_location.x) as usize][guard_direction.to_index()] = true;
+        visited[(guard_location.y * input.width + guard_location.x) as usize]
+            [guard_direction.to_index()] = true;
     }
 }
 
@@ -37,15 +41,48 @@ pub fn part1(input: &Grid<u8>) -> u32 {
     walk_grid(input).unwrap_or_default().len() as u32
 }
 
+fn is_loop(input: &Grid<u8>) -> bool {
+    let mut visited = FastSet::with_capacity((input.width * input.height) as usize);
+    let mut guard_location = input.find(b'^').unwrap();
+    let mut guard_direction = UP;
+
+    // Only add turning points to visited set
+    visited.insert((guard_location, guard_direction));
+
+    loop {
+        let mut new_location = guard_location;
+
+        // Continue walking until we hit a wall or need to turn
+        loop {
+            let next = new_location + Point::from(guard_direction);
+            if !input.contains(next) {
+                return false; // Found exit
+            }
+            if input[next] == b'#' {
+                guard_direction = guard_direction.clockwise();
+                // Only store location and direction when we turn
+                if visited.contains(&(new_location, guard_direction)) {
+                    return true; // Found a loop
+                }
+                visited.insert((new_location, guard_direction));
+                break;
+            }
+            new_location = next;
+        }
+        guard_location = new_location;
+    }
+}
+
 pub fn part2(input: &Grid<u8>) -> u32 {
     let initial_positions = walk_grid(input).unwrap_or_default();
-    
-    initial_positions.iter()
+
+    initial_positions
+        .iter()
         .filter(|&&p| input[p] == b'.')
         .filter(|&&p| {
             let mut modified_grid = input.clone();
             modified_grid[p] = b'#';
-            walk_grid(&modified_grid).is_none()
+            is_loop(&modified_grid)
         })
         .count() as u32
 }
