2d7b71040a50132d2b66141898acd1ed46300689
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-06 17:09:42 +0000

    Clean up part1 walk to be more readable and not use the array of booleans which was only slightly faster anyway

--- src/aoc2024/day06.rs
+++ src/aoc2024/day06.rs
@@ -5,41 +5,34 @@ pub fn parse(input: &str) -> Grid<u8> {
     Grid::parse(input)
 }
 
-fn walk_grid(input: &Grid<u8>) -> Option<FastSet<Point>> {
-    let mut visited = vec![[false; 4]; (input.width * input.height) as usize];
-    let mut positions = FastSet::with_capacity(20_000 as usize);
+fn walk_grid(input: &Grid<u8>) -> FastSet<Point> {
+    let mut positions = FastSet::with_capacity((input.width * input.height) as usize);
     let mut guard_location = input.find(b'^').unwrap();
     let mut guard_direction = UP;
 
     positions.insert(guard_location);
-    visited[(guard_location.y * input.width + guard_location.x) as usize]
-        [guard_direction.to_index()] = true;
 
     loop {
-        let new_location = guard_location + Point::from(guard_direction);
-        if !input.contains(new_location) {
-            return Some(positions);
-        }
+        let mut new_location = guard_location;
 
-        if input[new_location] == b'#' {
-            guard_direction = guard_direction.clockwise();
-        } else {
-            guard_location = new_location;
-            positions.insert(guard_location);
+        loop {
+            let next = new_location + Point::from(guard_direction);
+            if !input.contains(next) {
+                return positions;  // Found exit
+            }
+            if input[next] == b'#' {
+                guard_direction = guard_direction.clockwise();
+                break;
+            }
+            new_location = next;
+            positions.insert(new_location);
         }
-
-        if visited[(guard_location.y * input.width + guard_location.x) as usize]
-            [guard_direction.to_index()]
-        {
-            return None;
-        }
-        visited[(guard_location.y * input.width + guard_location.x) as usize]
-            [guard_direction.to_index()] = true;
+        guard_location = new_location;
     }
 }
 
 pub fn part1(input: &Grid<u8>) -> u32 {
-    walk_grid(input).unwrap_or_default().len() as u32
+    walk_grid(input).len() as u32
 }
 
 fn is_loop(input: &Grid<u8>, start_position: Point, blocked_point: Point) -> bool {
@@ -64,11 +57,6 @@ fn is_loop(input: &Grid<u8>, start_position: Point, blocked_point: Point) -> boo
                 }
                 visited.insert((new_location, guard_direction));
                 // This break ensures we don't move before finding a direction that has a free location
-                // e.g. in this scenario:
-                // .#.
-                // .^#
-                // ...
-                // The N is blocked, so he turns right, blocked again, but will not move into it, and turns South before moving.
                 break;
             }
             new_location = next;
@@ -81,17 +69,15 @@ pub fn part2(input: &Grid<u8>) -> u32 {
     let guard_pos = input.find(b'^').unwrap();
 
     // Use part1 to find all the locations the guard would walk, so we can put obstacles in their way!
-    let initial_positions: Vec<_> = walk_grid(input)
-        .unwrap_or_default()
-        .iter()
-        .filter(|&&p| p != guard_pos)
-        .copied()
-        .collect();
+    let mut initial_positions = walk_grid(input);
+    initial_positions.remove(&guard_pos);
 
     // Replace every visited location (except the start which we removed already) with a block and test if they walk in a loop
     // Uses par_iter to parallelize the loop detection. Reduces the time from 23,000us to 2,300us (10x roughly)
-    initial_positions.par_iter()
-        .with_min_len(5) // a batch size to optimize for parallelization
+    initial_positions.into_iter()
+        .collect::<Vec<_>>()
+        .par_iter()
+        // .with_min_len(50) // has some benefit to the parallelization if we batch - leaving as a comment for futre self
         .filter(|&p| is_loop(input, guard_pos, *p))
         .count() as u32
 }
