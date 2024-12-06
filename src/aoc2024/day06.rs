3c7e9164e3caaacfd55ea4f5dd4bde7d5e7e5d26
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-06 07:10:45 +0000

    2024 day 06 part 1 with some of part 2 refactoring already

--- src/aoc2024/day06.rs
+++ src/aoc2024/day06.rs
@@ -1,12 +1,50 @@
+use crate::util::{grid::*, point::*, hash::*};
 
-pub fn parse(_input: &str) -> Vec<u32> {
-    vec![]
+pub fn parse(input: &str) -> Grid<u8> {
+    Grid::parse(input)
 }
 
-pub fn part1(_input: &[u32]) -> u32 {
-    123
+fn walk_grid(input: &Grid<u8>) -> Result<u32, u32> {
+    let mut visited = FastSet::new();
+    let mut guard_location = input.find(b'^').unwrap();
+    let mut guard_direction = UP;
+    let mut is_in_grid = true;
+    
+    visited.insert((guard_location, guard_direction));
+
+    while is_in_grid {
+        let new_location = guard_location + Point::from(guard_direction);
+        if !input.contains(new_location) {
+            is_in_grid = false;
+            break;
+        }
+
+        if input[new_location] == b'#' {
+            guard_direction = guard_direction.clockwise();
+        } else {
+            guard_location = new_location;
+        }
+        
+        if !visited.insert((guard_location, guard_direction)) {
+            break;
+        }
+    }
+
+    let unique_positions: FastSet<_> = visited.iter()
+        .map(|(pos, _)| *pos)
+        .collect();
+    
+    if is_in_grid {
+        Err(unique_positions.len() as u32)
+    } else {
+        Ok(unique_positions.len() as u32)
+    }
 }
 
-pub fn part2(_input: &[u32]) -> u32 {
+pub fn part1(input: &Grid<u8>) -> u32 {
+    walk_grid(input).unwrap_or_default()
+}
+
+pub fn part2(_input: &Grid<u8>) -> u32 {
     456
 }
--- tests/aoc2024/day06_test.rs
+++ tests/aoc2024/day06_test.rs
@@ -1,15 +1,25 @@
 use aoc::aoc2024::day06::*;
 
-const EXAMPLE: &str = "";
+const EXAMPLE: &str = "\
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
 
 #[test]
 fn part1_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 123);
+    assert_eq!(part1(&input), 41);
 }
 
 #[test]
 fn part2_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part2(&input), 456);
+    assert_eq!(part2(&input), 6);
 }
