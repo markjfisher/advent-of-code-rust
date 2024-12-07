b7d663f5f61e1ebac958b4d5df58c7b452ac9fe0
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-07 08:15:41 +0000

    2024 day07 bridge repair - adding and multiplying numbers

--- src/aoc2024/day07.rs
+++ src/aoc2024/day07.rs
@@ -1,12 +1,82 @@
+use crate::util::parse::*;
 
-pub fn parse(_input: &str) -> Vec<u32> {
-    vec![]
+pub fn parse(input: &str) -> Vec<(u128, Vec<u128>)> {
+    input.lines().map(|line| {
+        // println!("{}", line);
+        let nums: Vec<u128> = line.iter_unsigned().collect();
+        // println!("{:?}", nums);
+        (nums[0], nums[1..].to_vec())
+    }).collect()
 }
 
-pub fn part1(_input: &[u32]) -> u32 {
-    123
+// p1: 5030892084481
+// p2: 91377501990670 too high
+// p2: 91377448645408 too high
+
+pub fn part1(input: &[(u128, Vec<u128>)]) -> u128 {
+    input.iter()
+        .filter(|(target, nums)| can_make_target(*target, nums, false))
+        .map(|(target, _)| target)
+        .sum()
 }
 
-pub fn part2(_input: &[u32]) -> u32 {
-    456
+pub fn part2(input: &[(u128, Vec<u128>)]) -> u128 {
+    input.iter()
+        .filter(|(target, nums)| can_make_target(*target, nums, true))
+        .map(|(target, _)| target)
+        .sum()
+}
+
+fn can_make_target(target: u128, nums: &[u128], use_concat: bool) -> bool {
+    let op_count = nums.len() - 1;
+    let bits_per_op = if use_concat { 2 } else { 1 };
+    let combinations = 1 << (bits_per_op * op_count);
+
+    for op_combination in 0..combinations {
+        let mut result = nums[0];
+        // let mut expression = format!("{}", nums[0]);
+        
+        for i in 0..op_count {
+            let next_num = nums[i + 1];
+            let op_bits = if use_concat {
+                (op_combination >> (2 * i)) & 0b11
+            } else {
+                (op_combination >> i) & 0b1
+            };
+
+            // let (op, result_new) = if use_concat {
+            let (_, result_new) = if use_concat {
+                match op_bits {
+                    0 => ('+', result + next_num),
+                    1 => ('*', result * next_num),
+                    2 => ('|', {
+                        let concat = format!("{}{}", result, next_num)
+                            .parse::<u128>()
+                            .unwrap_or(0);
+                        concat
+                    }),
+                    _ => continue,
+                }
+            } else {
+                match op_bits {
+                    0 => ('+', result + next_num),
+                    1 => ('*', result * next_num),
+                    _ => unreachable!(),
+                }
+            };
+            
+            // expression.push_str(&format!(" {} {}", op, next_num));
+            result = result_new;
+        }
+        
+        if result == target {
+            // println!("Found target {}: {} = {}", target, expression, result);
+            return true;
+        } else {
+            // println!("No match for {}: {} = {}", target, expression, result);
+        }
+    }
+    
+    // println!("Target {} not possible after trying all combinations", target);
+    false
 }
--- tests/aoc2024/day07_test.rs
+++ tests/aoc2024/day07_test.rs
@@ -1,15 +1,31 @@
 use aoc::aoc2024::day07::*;
 
-const EXAMPLE: &str = "";
+const EXAMPLE: &str = "\
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
+
+const EXAMPLE2: &str = "\
+432832280199: 3 286 4 3 17 682 7 7 9 2
+6606690226623: 605 42 3 47 26 6 622
+4920367: 8 8 896 6 4 955 9 44 6 9
+221825533: 124 5 714 905 66 5 33
+";
 
 #[test]
 fn part1_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 123);
+    assert_eq!(part1(&input), 3749);
 }
 
 #[test]
 fn part2_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part2(&input), 456);
+    assert_eq!(part2(&input), 11387);
 }
