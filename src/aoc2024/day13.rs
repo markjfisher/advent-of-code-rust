ae291167489b69e7e617b7c8f3c65bfee9d03e7b
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-13 17:00:59 +0000

    small optimization to double check x and y are integers using modulus first before division

--- src/aoc2024/day13.rs
+++ src/aoc2024/day13.rs
@@ -8,6 +8,9 @@ pub fn parse(input: &str) -> Vec<Vec<u64>> {
 }
 
 // just do linear algebra. very simple day
+// For both x, and y (our variables) we know they are integers, so they must
+// divide perfectly. Thus we can do modulus checks before dividing. If the remainder isn't 0
+// then we know the solution is not an integer, which means when it is, we don't worry about floats and rounding errors.
 fn solve_equations(coeffs: &[u64]) -> Option<(i64, i64)> {
     let a = coeffs[0] as i64;
     let b = coeffs[2] as i64;
@@ -16,22 +19,24 @@ fn solve_equations(coeffs: &[u64]) -> Option<(i64, i64)> {
     let e = coeffs[4] as i64;
     let f = coeffs[5] as i64;
 
-    let denominator = b * c - a * d;
-    if denominator == 0 {
+    let x_denom = b * c - a * d;
+    if x_denom == 0 {
+        // sanity check the determinant is not going to be 0 (e.g. equations are not linearly independent)
         return None;
     }
 
-    let numerator = f * b - d * e;
-    if numerator % denominator != 0 {
+    let x_numerator = f * b - d * e;
+    if x_numerator % x_denom != 0 {
         return None;
     }
+    let x = x_numerator / x_denom;
 
-    let x = numerator / denominator;
-    let y = (e - a * x) / b;
-
-    if c * x + d * y != f {
+    let y_numerator = e - a * x;
+    // in our input, b is never 0, so we the division is safe
+    if y_numerator % b != 0 {
         return None;
     }
+    let y = y_numerator / b;
 
     Some((x, y))
 }
