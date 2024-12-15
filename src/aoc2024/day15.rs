fb1ddec24d29df6160f765cd596a0e28d3642663
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-15 15:03:03 +0000

    2024 day15 sokoban box moving

--- src/aoc2024/day15.rs
+++ src/aoc2024/day15.rs
@@ -1,12 +1,192 @@
+use crate::util::grid::*;
+use crate::util::point::*;
+use std::mem::swap;
 
-pub fn parse(_input: &str) -> Vec<u32> {
-    vec![]
+pub fn parse(input: &str) -> (Grid<u8>, Vec<Point>) {
+    let (g, m) = input.split_once("\n\n").unwrap();
+    let grid = Grid::parse(g);
+    let moves: Vec<Point> = m
+        .chars()
+        .filter(|&c| c != '\n' && c != '\r')
+        .map(|c| Point::from(c as u8))
+        .collect();
+
+    (grid, moves)
 }
 
-pub fn part1(_input: &[u32]) -> u32 {
-    123
+pub fn part1(input: &(Grid<u8>, Vec<Point>)) -> u32 {
+    let (grid, moves) = input;
+    let mut grid = grid.clone();
+    do_moves(&mut grid, &moves);
+    score_grid(&grid, b'O')
 }
 
-pub fn part2(_input: &[u32]) -> u32 {
-    456
+pub fn move_object(grid: &mut Grid<u8>, pos: Point, dir: Point, object: u8) -> bool {
+    let new_pos = pos + dir;
+
+    // println!("\nmove_object: {:?} {:?} {}", pos, dir, object as char);
+    // println!("new_pos: {:?}, grid[new_pos]: {}", new_pos, grid[new_pos] as char);
+    match grid[new_pos] {
+        b'#' => false, // Can't move into walls
+        b'.' => {
+            // Can move into empty space
+            grid[pos] = b'.'; // Clear old position
+            grid[new_pos] = object; // Move object to new position
+            true
+        }
+        b'O' => {
+            // Try to push box
+            // println!("pushing box");
+            if move_object(grid, new_pos, dir, b'O') {
+                // println!("box moved");
+                grid[pos] = b'.';
+                grid[new_pos] = object;
+                true
+            } else {
+                // println!("FAILED to move box");
+                false
+            }
+        }
+        _ => false,
+    }
+}
+
+pub fn do_moves(grid: &mut Grid<u8>, moves: &Vec<Point>) {
+    let mut robot = grid.find(b'@').unwrap();
+    for &mv in moves {
+        if move_object(grid, robot, mv, b'@') {
+            robot = robot + mv;
+        }
+    }
+}
+
+pub fn score_grid(grid: &Grid<u8>, box_char: u8) -> u32 {
+    grid.points()
+        .filter(|p| grid[*p] == box_char)
+        .map(|p| (p.x + p.y * 100) as u32)
+        .sum()
+}
+
+// This time use bfs to move boxes rather than recursion as in part 1
+pub fn part2(input: &(Grid<u8>, Vec<Point>)) -> u32 {
+    let (grid, moves) = input;
+
+    let mut grid = wide_grid(grid);
+    let mut robot = grid.find(b'@').unwrap();
+
+    do_wide_moves(&mut grid, &mut robot, moves);
+    score_grid(&grid, b'[')
+}
+
+// convert from normal grid to wide box version for part 2
+pub fn wide_grid(grid: &Grid<u8>) -> Grid<u8> {
+    let mut next = Grid::new(grid.width * 2, grid.height, b'.');
+
+    for y in 0..grid.height {
+        for x in 0..grid.width {
+            let (left, right) = match grid[Point::new(x, y)] {
+                b'@' => (b'@', b'.'),
+                b'O' => (b'[', b']'),
+                b'#' => (b'#', b'#'),
+                _ => continue,
+            };
+
+            next[Point::new(2 * x, y)] = left;
+            next[Point::new(2 * x + 1, y)] = right;
+        }
+    }
+
+    next
+}
+
+fn narrow(grid: &mut Grid<u8>, start: &mut Point, dir: Point) {
+    let mut pos = *start + dir;
+    let mut size = 2;
+
+    while grid[pos] != b'.' && grid[pos] != b'#' {
+        pos += dir;
+        size += 1;
+    }
+
+    if grid[pos] == b'.' {
+        let mut previous = b'.';
+        let mut position = *start;
+
+        for _ in 0..size {
+            swap(&mut previous, &mut grid[position]);
+            position += dir;
+        }
+
+        *start += dir;
+    }
+}
+
+fn wide(
+    grid: &mut Grid<u8>,
+    start: &mut Point,
+    dir: Point,
+    todo: &mut Vec<Point>,
+    seen: &mut Grid<usize>,
+    id: usize,
+) {
+    let pos = *start;
+    let next = pos + dir;
+
+    if grid[next] == b'.' {
+        grid[pos] = b'.';
+        grid[next] = b'@';
+        *start += dir;
+        return;
+    }
+
+    todo.clear();
+    todo.push(*start);
+    let mut index = 0;
+
+    while index < todo.len() {
+        let next = todo[index] + dir;
+        index += 1;
+
+        let other = match grid[next] {
+            b'#' => return,
+            b'[' => RIGHT,
+            b']' => LEFT,
+            _ => continue,
+        };
+
+        // split the box into two halves
+        let first = next;
+        if seen[first] != id {
+            seen[first] = id;
+            todo.push(first);
+        }
+
+        let second = next + other;
+        if seen[second] != id {
+            seen[second] = id;
+            todo.push(second);
+        }
+    }
+
+    for &point in todo.iter().rev() {
+        grid[point + dir] = grid[point];
+        grid[point] = b'.';
+    }
+
+    *start += dir;
+}
+
+pub fn do_wide_moves(grid: &mut Grid<u8>, robot: &mut Point, moves: &Vec<Point>) {
+    let mut todo = Vec::new();
+    let mut seen = grid.same_size_with(usize::MAX);
+
+    for (id, p) in moves.into_iter().enumerate() {
+        match *p {
+            LEFT => narrow(grid, robot, LEFT),
+            RIGHT => narrow(grid, robot, RIGHT),
+            UP => wide(grid, robot, UP, &mut todo, &mut seen, id),
+            DOWN => wide(grid, robot, DOWN, &mut todo, &mut seen, id),
+            _ => (),
+        }
+    }
 }
--- tests/aoc2024/day15_test.rs
+++ tests/aoc2024/day15_test.rs
@@ -1,15 +1,347 @@
 use aoc::aoc2024::day15::*;
+use aoc::util::point::*;
+use indoc::indoc;
+use pretty_assertions::assert_eq;
 
-const EXAMPLE: &str = "";
+const EXAMPLE1: &str = "\
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<";
+
+const EXAMPLE2: &str = "\
+##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
 
 #[test]
 fn part1_test() {
-    let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 123);
+    assert_eq!(part1(&parse(EXAMPLE1)), 2028);
+    assert_eq!(part1(&parse(EXAMPLE2)), 10092);
 }
 
 #[test]
 fn part2_test() {
-    let input = parse(EXAMPLE);
-    assert_eq!(part2(&input), 456);
+    assert_eq!(part2(&parse(EXAMPLE2)), 9021);
+}
+
+#[test]
+fn parse_test1() {
+    let (grid, moves) = parse(EXAMPLE1);
+    assert_eq!(grid.width, 8);
+    assert_eq!(grid.height, 8);
+    assert_eq!(moves.len(), 15);
+
+    assert_eq!(grid[Point::new(0, 0)], b'#');
+    assert_eq!(grid[Point::new(2, 2)], b'@');
+}
+
+#[test]
+fn parse_test2() {
+    let (grid, moves) = parse(EXAMPLE2);
+    assert_eq!(grid.width, 10);
+    assert_eq!(grid.height, 10);
+    assert_eq!(moves.len(), 700);
+    assert_eq!(grid[Point::new(0, 0)], b'#');
+    assert_eq!(grid[Point::new(4, 4)], b'@');
+}
+
+#[test]
+fn move_test_individual_moves() {
+    let (mut grid, _moves) = parse(EXAMPLE1);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #..O.O.#
+        ##@.O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########"});
+    do_moves(&mut grid, &vec![LEFT]);
+
+    // first move is blocked by wall
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #..O.O.#
+        ##@.O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########"});
+
+    // second move is up into space
+    do_moves(&mut grid, &vec![UP]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #.@O.O.#
+        ##..O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########"});
+
+    // 3rd move is up - hits wall
+    do_moves(&mut grid, &vec![UP]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #.@O.O.#
+        ##..O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########"});
+
+    // 4th move is right - pushes 1 box
+    do_moves(&mut grid, &vec![RIGHT]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #..@OO.#
+        ##..O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########"});
+
+    // 5th move is right - pushes 2 boxes
+    do_moves(&mut grid, &vec![RIGHT]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #...@OO#
+        ##..O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########"});
+
+    // 6th move is down - pushes 4 boxes
+    do_moves(&mut grid, &vec![DOWN]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##..@..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #...O..#
+        ########"});
+
+    // 7th move is down - pushes nothing
+    do_moves(&mut grid, &vec![DOWN]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##..@..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #...O..#
+        ########"});
+
+    // 8th move is left into space
+    do_moves(&mut grid, &vec![LEFT]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##.@...#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #...O..#
+        ########"});
+
+    // 9th move is down into space
+    do_moves(&mut grid, &vec![DOWN]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##.....#
+        #..@O..#
+        #.#.O..#
+        #...O..#
+        #...O..#
+        ########"});
+
+    // 10th move is RIGHT moving 1 box
+    do_moves(&mut grid, &vec![RIGHT]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##.....#
+        #...@O.#
+        #.#.O..#
+        #...O..#
+        #...O..#
+        ########"});
+
+    // 11th move is RIGHT moving 1 box
+    do_moves(&mut grid, &vec![RIGHT]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##.....#
+        #....@O#
+        #.#.O..#
+        #...O..#
+        #...O..#
+        ########"});
+
+    // 12th move is down into space
+    do_moves(&mut grid, &vec![DOWN]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##.....#
+        #.....O#
+        #.#.O@.#
+        #...O..#
+        #...O..#
+        ########"});
+
+    // 13th move is left moving 1 box
+    do_moves(&mut grid, &vec![LEFT]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##.....#
+        #.....O#
+        #.#O@..#
+        #...O..#
+        #...O..#
+        ########"});
+
+    // 14th move is left moving nothing
+    do_moves(&mut grid, &vec![LEFT]);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##.....#
+        #.....O#
+        #.#O@..#
+        #...O..#
+        #...O..#
+        ########"});
+
+}
+
+#[test]
+fn move_test_all_together() {
+    let (mut grid, moves) = parse(EXAMPLE1);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #..O.O.#
+        ##@.O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########"});
+    do_moves(&mut grid, &moves);
+
+    // after all moves, should be same as first test done individually
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ########
+        #....OO#
+        ##.....#
+        #.....O#
+        #.#O@..#
+        #...O..#
+        #...O..#
+        ########"});
+
+}
+
+#[test]
+fn move_larger_test() {
+    let (mut grid, moves) = parse(EXAMPLE2);
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ##########
+        #..O..O.O#
+        #......O.#
+        #.OO..O.O#
+        #..O@..O.#
+        #O#..O...#
+        #O..O..O.#
+        #.OO.O.OO#
+        #....O...#
+        ##########"});
+    do_moves(&mut grid, &moves);
+
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ##########
+        #.O.O.OOO#
+        #........#
+        #OO......#
+        #OO@.....#
+        #O#.....O#
+        #O.....OO#
+        #O.....OO#
+        #OO....OO#
+        ##########"});
+}
+
+#[test]
+fn can_widen_grid() {
+    let (grid, _) = parse(EXAMPLE2);
+    let wide_grid = wide_grid(&grid);
+    assert_eq!(wide_grid.to_grid_string(), indoc! {"\
+        ####################
+        ##....[]....[]..[]##
+        ##............[]..##
+        ##..[][]....[]..[]##
+        ##....[]@.....[]..##
+        ##[]##....[]......##
+        ##[]....[]....[]..##
+        ##..[][]..[]..[][]##
+        ##........[]......##
+        ####################"});
+}
+
+#[test]
+fn can_do_wide_moves() {
+    let (grid, moves) = parse(EXAMPLE2);
+    let mut wide_grid = wide_grid(&grid);
+    let mut robot = wide_grid.find(b'@').unwrap();
+    do_wide_moves(&mut wide_grid, &mut robot, &moves);
+
+    assert_eq!(wide_grid.to_grid_string(), indoc! {"\
+        ####################
+        ##[].......[].[][]##
+        ##[]...........[].##
+        ##[]........[][][]##
+        ##[]......[]....[]##
+        ##..##......[]....##
+        ##..[]............##
+        ##..@......[].[][]##
+        ##......[][]..[]..##
+        ####################"});
 }
