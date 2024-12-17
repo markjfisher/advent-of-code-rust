1fa59e43244c70edbe3cb0bf44612eeaf46ea2d4
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-17 16:05:29 +0000

    read bxl constants from the program data to make this generic

--- src/aoc2024/day17.rs
+++ src/aoc2024/day17.rs
@@ -15,7 +15,7 @@ pub fn part1(input: &[usize]) -> String {
 // we see that the nth digit increments at every 8^nth step, so we have to find a value for last digit, then move backwards until they all match.
 // We're effectively shifting the solution by powers of 8 after finding each number.
 
-// This version uses the Comp to run the simulation so works on any input
+// This version uses the Comp to run the simulation so works on any input. It's slower than direct, as it doesn't shortcut values when they don't match expected output
 pub fn _part2(input: &[usize]) -> usize {
     let program = &input[3..];
     let mut a = 0;
@@ -45,11 +45,17 @@ pub fn _part2(input: &[usize]) -> usize {
     a
 }
 
-// This version is optimized to my input by manually working out the bitwise operations instead of using the Comp
-// but still using the reverse digit lookup idea
+// This version is optimized to do instructions manually rather than using the Comp
+// but still using the reverse digit lookup idea. It gets the 2 bxl constants from the input by finding the first two values in a pair that match [1, bxl1], [1, bxl2]
 pub fn part2(input: &[usize]) -> usize {
     let program = &input[3..];
     let mut a = 0;
+
+    let [bxl1, bxl2] = program.chunks(2)
+        .filter(|p| p[0] == 1)
+        .map(|p| p[1])
+        .take(2)
+        .collect::<Vec<usize>>()[..] else { panic!("Did not find two bxl commands") };
 
     for n in 1..=program.len() {
         let target = program[program.len()-n..].to_vec();
@@ -59,26 +65,23 @@ pub fn part2(input: &[usize]) -> usize {
             let mut digits = Vec::new();
             let mut test_a = new_a;
 
-            // I tried memoizing the values here, but it's marginally faster to keep calculating
-            // the same values rather than looking up in a map
             while test_a != 0 {
                 let mut b = test_a & 0x07;
-                b = b ^ 1;
+                b = b ^ bxl1;
                 let c = test_a >> b;
-                b = b ^ 5;
-                test_a >>= 3;
                 b = b ^ c;
+                b = b ^ bxl2;
+                test_a >>= 3;
 
                 let test_digit = b & 0x07;
                 if test_digit != *target.get(digits.len()).unwrap() {
+                    // skip if the new digit doesn't match the target.
                     break;
                 }
 
                 digits.push(test_digit);
-                // println!("test_a: {}, b: {}, c: {}, pushing {}", test_a, b, c, b & 0x07);
             }
 
-            // println!("digits: {:?}, target: {:?}", digits, target);
             if digits == target {
                 a = new_a;
                 break;
