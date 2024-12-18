82a2bed7d616b85774c88fade2c9eaeb04ca0921
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-18 10:24:43 +0000

    2024 day18 corrupt memory falling blocks - easy dijkstra

--- src/aoc2024/day18.rs
+++ src/aoc2024/day18.rs
@@ -1,12 +1,58 @@
+use crate::util::grid::*;
+use crate::util::iter::*;
+use crate::util::parse::*;
+use crate::util::point::*;
+use pathfinding::prelude::dijkstra;
 
-pub fn parse(_input: &str) -> Vec<u32> {
-    vec![]
+pub fn parse(input: &str) -> Vec<(i32, i32)> {
+    input.iter_unsigned().chunk::<2>().map(|c: [u32; 2]| (c[0] as i32, c[1] as i32)).collect()
 }
 
-pub fn part1(_input: &[u32]) -> u32 {
-    123
+pub fn part1(input: &[(i32, i32)]) -> u32 {
+    let grid: Grid<u8> = create_grid(input, 71, 71, 1024);
+    shortest_path(&grid).unwrap().1
 }
 
-pub fn part2(_input: &[u32]) -> u32 {
-    456
+// brute force find the first block that blocks the path using dijkstra instead of A* in day16
+pub fn part2(input: &[(i32, i32)]) -> String {
+    let mut grid: Grid<u8> = create_grid(input, 71, 71, 1024);
+
+    // Create iterator over the remaining blocks
+    for &next_block in input.iter().skip(1024) {
+        grid[Point::new(next_block.0, next_block.1)] = b'#';
+
+        if shortest_path(&grid).is_none() {
+            // Path is blocked, return the coordinates as a comma-separated string
+            return format!("{},{}", next_block.0, next_block.1);
+        }
+    }
+
+    "0,0".to_string() // Fallback return if no blocking occurs rather than throwing an exception
+}
+
+pub fn create_grid(input: &[(i32, i32)], width: i32, height: i32, count: usize) -> Grid<u8> {
+    let mut grid = Grid::new(width, height, b'.');
+    for (x, y) in input.iter().take(count) {
+        grid[Point::new(*x, *y)] = b'#';
+    }
+    grid
+}
+
+pub fn shortest_path(grid: &Grid<u8>) -> Option<(Vec<Point>, u32)> {
+    let start = Point::new(0, 0);   
+    let end = Point::new(grid.width - 1, grid.height - 1);
+    let x: Option<(Vec<Point>, u32)> = dijkstra(
+        &start,
+        |p| ORTHOGONAL.iter()
+            .filter_map(|d| {
+                let next = *p + *d;
+                if grid.contains(next) && grid[next] != b'#' {
+                    Some((next, 1u32)) // simple cost
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>(),
+        |p| *p == end);
+    x
 }
--- tests/aoc2024/day18_test.rs
+++ tests/aoc2024/day18_test.rs
@@ -1,15 +1,61 @@
 use aoc::aoc2024::day18::*;
+use pretty_assertions::assert_eq;
+use indoc::indoc;
 
-const EXAMPLE: &str = "";
+const INPUT1: &str = "\
+5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0";
 
 #[test]
 fn part1_test() {
-    let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 123);
+    let input = parse(INPUT1);
+    let grid = create_grid(&input, 7, 7, 12);
+    assert_eq!(shortest_path(&grid).unwrap().1, 22);
 }
 
 #[test]
 fn part2_test() {
-    let input = parse(EXAMPLE);
+    let input = parse(INPUT1);
     assert_eq!(part2(&input), 456);
 }
+
+#[test]
+fn create_grid_test() {
+    let input = parse(INPUT1);
+    let grid = create_grid(&input, 7, 7, 12);
+    assert_eq!(grid.width, 7);
+    assert_eq!(grid.height, 7);
+    // println!("{}", grid.to_grid_string());
+    assert_eq!(grid.to_grid_string(), indoc! {"\
+        ...#...
+        ..#..#.
+        ....#..
+        ...#..#
+        ..#..#.
+        .#..#..
+        #.#...."});
+}
+
