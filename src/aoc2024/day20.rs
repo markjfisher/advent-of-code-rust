6e07bd1e6ca902e94ac6cc09146761fedea8bb9b
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-20 13:51:39 +0000

    2024 day 20 consolidated version for both parts after reworking solution

--- src/aoc2024/day20.rs
+++ src/aoc2024/day20.rs
@@ -1,12 +1,85 @@
+use crate::util::grid::*;
+use crate::util::point::*;
+use std::collections::VecDeque;
 
-pub fn parse(_input: &str) -> Vec<u32> {
-    vec![]
+pub fn parse(input: &str) -> Grid<usize> {
+    bfs_times(&Grid::parse(input))
 }
 
-pub fn part1(_input: &[u32]) -> u32 {
-    123
+// find the time it takes to reach each every point from start
+// walls will stay as time 0, which means they aren't on the path
+pub fn bfs_times(grid: &Grid<u8>) -> Grid<usize> {
+    let start = grid.find(b'S').unwrap();
+    let mut grid_bfs_time = grid.same_size_with(0_usize);
+    let mut queue = VecDeque::new();
+    
+    grid_bfs_time[start] = 1;
+    queue.push_back(start);
+    let mut step_time = 0;
+
+    // simple bfs
+    while let Some(current) = queue.pop_front() {
+        step_time += 1;
+        for &dir in ORTHOGONAL.iter() {
+            let next = current + dir;
+            if grid.contains(next) && grid[next] != b'#' && grid_bfs_time[next] == 0 {
+                queue.push_back(next);
+                // we need to offset since start time is 1, so we can check 0 value where not yet visited
+                grid_bfs_time[next] = step_time + 1;
+            }
+        }
+    }
+    
+    grid_bfs_time
 }
 
-pub fn part2(_input: &[u32]) -> u32 {
-    456
+pub fn count_improvements(
+    grid_bfs_time: &Grid<usize>,
+    max_range: i32,
+    min_improvement: usize,
+) -> (u32, u32) {
+    let mut improvements_p1 = 0;
+    let mut improvements_p2 = 0;
+    let mut step_time = 0;
+
+    // get all the path points from the grid so we can iterate over them
+    let path_points: Vec<_> = grid_bfs_time.points()
+        .filter(|&p| grid_bfs_time[p] > 0)
+        .collect();
+
+    while step_time < path_points.len() {
+        let current = path_points[step_time];
+        
+        // look around the current point to see if we connect with somewhere else that's better than our improvement minimum
+        let current_step_time = grid_bfs_time[current];
+        for dy in -max_range..=max_range {
+            for dx in (-max_range + dy.abs())..=(max_range - dy.abs()) {
+                let connection_distance = (dx.abs() + dy.abs()) as usize;
+                let next = current + Point::new(dx, dy);
+                
+                // check if the next point is on the path
+                let next_step_time = if grid_bfs_time.contains(next) { grid_bfs_time[next] } else { 0 };
+                // do we make enough of an improvement if we can get to the next point?
+                // note: saturating_sub is used to avoid having to do "if next_step_time > current_step_time" and returns 0 for that case, as the types are usize we can't simply subtract
+                if next_step_time.saturating_sub(current_step_time) >= connection_distance + min_improvement {
+                    if connection_distance == 2 {
+                        improvements_p1 += 1;
+                    }
+                    improvements_p2 += 1;
+                }
+            }
+        }
+        
+        step_time += 1;
+    }
+
+    (improvements_p1, improvements_p2)
+}
+
+pub fn part1(bfs_grid: &Grid<usize>) -> u32 {
+    count_improvements(&bfs_grid, 20, 100).0
+}
+
+pub fn part2(bfs_grid: &Grid<usize>) -> u32 {
+    count_improvements(&bfs_grid, 20, 100).1
 }
--- tests/aoc2024/day20_test.rs
+++ tests/aoc2024/day20_test.rs
@@ -1,15 +1,53 @@
 use aoc::aoc2024::day20::*;
 
-const EXAMPLE: &str = "";
+const EXAMPLE: &str = "\
+###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############";
 
 #[test]
 fn part1_test() {
-    let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 123);
+    let bfs_times = parse(EXAMPLE);
+    assert_eq!(count_improvements(&bfs_times, 2, 64).0, 1);
+    assert_eq!(count_improvements(&bfs_times, 2, 40).0, 2);
+    assert_eq!(count_improvements(&bfs_times, 2, 38).0, 3);
+    assert_eq!(count_improvements(&bfs_times, 2, 36).0, 4);
+    assert_eq!(count_improvements(&bfs_times, 2, 20).0, 5);
+    assert_eq!(count_improvements(&bfs_times, 2, 12).0, 8);
+    assert_eq!(count_improvements(&bfs_times, 2, 10).0, 10);
+    assert_eq!(count_improvements(&bfs_times, 2, 8).0, 14);
+    assert_eq!(count_improvements(&bfs_times, 2, 6).0, 16);
+    assert_eq!(count_improvements(&bfs_times, 2, 4).0, 30);
+    assert_eq!(count_improvements(&bfs_times, 2, 2).0, 44);
 }
 
 #[test]
 fn part2_test() {
-    let input = parse(EXAMPLE);
-    assert_eq!(part2(&input), 456);
+    let bfs_times = parse(EXAMPLE);
+    assert_eq!(count_improvements(&bfs_times, 20, 76).1, 3);
+    assert_eq!(count_improvements(&bfs_times, 20, 74).1, 7);
+    assert_eq!(count_improvements(&bfs_times, 20, 72).1, 29);
+    assert_eq!(count_improvements(&bfs_times, 20, 70).1, 41);
+    assert_eq!(count_improvements(&bfs_times, 20, 68).1, 55);
+    assert_eq!(count_improvements(&bfs_times, 20, 66).1, 67);
+    assert_eq!(count_improvements(&bfs_times, 20, 64).1, 86);
+    assert_eq!(count_improvements(&bfs_times, 20, 62).1, 106);
+    assert_eq!(count_improvements(&bfs_times, 20, 60).1, 129);
+    assert_eq!(count_improvements(&bfs_times, 20, 58).1, 154);
+    assert_eq!(count_improvements(&bfs_times, 20, 56).1, 193);
+    assert_eq!(count_improvements(&bfs_times, 20, 54).1, 222);
+    assert_eq!(count_improvements(&bfs_times, 20, 52).1, 253);
+    assert_eq!(count_improvements(&bfs_times, 20, 50).1, 285);
 }
