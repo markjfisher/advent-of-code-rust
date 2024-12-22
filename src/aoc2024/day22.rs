a9109cfc7c807a224ecc4b1a0dbf5afc0219de75
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-22 09:15:47 +0000

    2024 day 22 - monkey business

--- src/aoc2024/day22.rs
+++ src/aoc2024/day22.rs
@@ -1,12 +1,26 @@
+use crate::util::parse::ParseOps;
 
-pub fn parse(_input: &str) -> Vec<u32> {
-    vec![]
+
+pub fn parse(input: &str) -> Vec<usize> {
+    input.iter_unsigned().collect()
 }
 
-pub fn part1(_input: &[u32]) -> u32 {
-    123
+pub fn part1(input: &[usize]) -> usize {
+    input.iter()
+        .map(|&number| (0..2000).fold(number, |n, _| gen(n)))
+        .sum()
 }
 
-pub fn part2(_input: &[u32]) -> u32 {
+pub fn part2(_input: &[usize]) -> u32 {
     456
+}
+
+pub fn gen(mut n: usize) -> usize {
+    n ^= n << 6;
+    n &= 0xffffff;
+    n ^= n >> 5;
+    n &= 0xffffff;
+    n ^= n << 11;
+    n &= 0xffffff;
+    n
 }
--- tests/aoc2024/day22_test.rs
+++ tests/aoc2024/day22_test.rs
@@ -1,15 +1,19 @@
 use aoc::aoc2024::day22::*;
 
-const EXAMPLE: &str = "";
+const EXAMPLE1: &str = "\
+1
+10
+100
+2024";
 
 #[test]
 fn part1_test() {
-    let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 123);
+    let input = parse(EXAMPLE1);
+    assert_eq!(part1(&input), 37327623);
 }
 
 #[test]
 fn part2_test() {
-    let input = parse(EXAMPLE);
+    let input = parse(EXAMPLE1);
     assert_eq!(part2(&input), 456);
 }
