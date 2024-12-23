fbb4aa9b54f559457a91be540a045e0c75566b73
Mark Fisher <mark.j.fisher@gmail.com>
2024-12-23 11:43:30 +0000

    2024 day23 part1

--- src/aoc2024/day23.rs
+++ src/aoc2024/day23.rs
@@ -1,12 +1,51 @@
+use crate::util::hash::*;
 
-pub fn parse(_input: &str) -> Vec<u32> {
-    vec![]
+type Input<'a> = FastMap<&'a str, FastSet<&'a str>>;
+
+pub fn parse(input: &str) -> Input<'_> {
+    input.lines().fold(FastMap::new(), |mut map, line| {
+        let (a, b) = line.split_once('-').unwrap();
+        map.entry(a).or_insert_with(FastSet::new).insert(b);
+        map.entry(b).or_insert_with(FastSet::new).insert(a);
+        map
+    })
 }
 
-pub fn part1(_input: &[u32]) -> u32 {
-    123
+pub fn part1(input: &Input<'_>) -> usize {
+    let mut count = 0;
+    
+    // For each node that starts with 't'
+    for (&name, connections) in input {
+        if !name.starts_with('t') {
+            continue;
+        }
+        
+        // For each pair of connections from our 't' node
+        for &first in connections {
+            for &second in connections {
+                // Skip if either:
+                // 1. first >= second (avoid duplicate pairs)
+                // 2. either connection starts with 't' and comes before our current name
+                //    (avoid counting the same triple from multiple 't' perspectives)
+                if first >= second || 
+                   (first.starts_with('t') && first < name) ||
+                   (second.starts_with('t') && second < name) {
+                    continue;
+                }
+                
+                // If these two nodes are also connected, we found a triple
+                if input.get(first)
+                       .map_or(false, |set| set.contains(second)) {
+                    println!("{} {} {}", name, first, second);
+                    count += 1;
+                }
+            }
+        }
+    }
+    
+    count
 }
 
-pub fn part2(_input: &[u32]) -> u32 {
+pub fn part2(_input: &Input<'_>) -> u32 {
     456
 }
--- tests/aoc2024/day23_test.rs
+++ tests/aoc2024/day23_test.rs
@@ -1,15 +1,114 @@
 use aoc::aoc2024::day23::*;
+use assert_unordered::assert_eq_unordered;
+use aoc::util::hash::*;
 
-const EXAMPLE: &str = "";
+const EXAMPLE: &str = "\
+kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn";
 
 #[test]
 fn part1_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 123);
+    assert_eq!(part1(&input), 7);
 }
 
 #[test]
 fn part2_test() {
     let input = parse(EXAMPLE);
     assert_eq!(part2(&input), 456);
+}
+
+#[test]
+fn parse_test() {
+    let input = parse(EXAMPLE);
+    // 16 unique names: aq,cg,co,de,ka,kh,qp,ta,tb,tc,td,ub,vc,wh,wq,yn
+    assert_eq!(input.len(), 16);
+
+    // looking at tX connections:
+    // ta-co
+    // ta-ka
+    // de-ta
+    // kh-ta
+    // ^^^^^^^^^^ ta
+    // cg-tb
+    // tb-ka
+    // tb-wq
+    // tb-vc
+    // ^^^^^^^^^^ tb
+    // kh-tc
+    // wh-tc
+    // tc-td
+    // co-tc
+    // ^^^^^^^^^^ tc
+    // tc-td
+    // wh-td
+    // td-qp
+    // td-yn
+    // ^^^^^^^^^^ td
+
+    assert_eq_unordered!(input.get("ta").unwrap(), &FastSet::from_iter(vec!["co", "ka", "de", "kh"]));
+    assert_eq_unordered!(input.get("tb").unwrap(), &FastSet::from_iter(vec!["cg", "ka", "wq", "vc"]));
+    assert_eq_unordered!(input.get("tc").unwrap(), &FastSet::from_iter(vec!["kh", "wh", "td", "co"]));
+    assert_eq_unordered!(input.get("td").unwrap(), &FastSet::from_iter(vec!["tc", "wh", "qp", "yn"]));
+
+    // de connections:
+    // de-cg
+    // de-co
+    // de-ta
+    // ka-de
+    // ^^^^^^^^^^ de
+    assert_eq_unordered!(input.get("de").unwrap(), &FastSet::from_iter(vec!["cg", "co", "ta", "ka"]));
+
+    // ka connections:
+    // ka-co
+    // tb-ka
+    // ta-ka
+    // ka-de
+    // ^^^^^^^^^^ ka
+    assert_eq_unordered!(input.get("ka").unwrap(), &FastSet::from_iter(vec!["co", "tb", "ta", "de"]));
+
+    // ka-co
+    // ta-co
+    // de-co
+    // co-tc
+    // ^^^^^^^^^^ co
+    assert_eq_unordered!(input.get("co").unwrap(), &FastSet::from_iter(vec!["ka", "ta", "de", "tc"]));
+
+    // kh-tc
+    // qp-kh
+    // kh-ub
+    // kh-ta
+    // ^^^^^^^^^^ kh
+    assert_eq_unordered!(input.get("kh").unwrap(), &FastSet::from_iter(vec!["tc", "qp", "ub", "ta"]));
+
 }
