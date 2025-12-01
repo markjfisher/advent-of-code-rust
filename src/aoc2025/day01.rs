c423b56bc82d09002ee2b02b59b3796c04224c2e
Mark Fisher <mark.j.fisher@gmail.com>
2025-12-01 10:21:35 +0000

    2025 day01 part 2

--- src/aoc2025/day01.rs
+++ src/aoc2025/day01.rs
@@ -1,27 +1,34 @@
-pub fn parse(input: &str) -> Vec<u32> {
+pub fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
+    do_rotations(input, 50) 
+}
+
+pub fn part1(input: &(Vec<u32>, Vec<u32>)) -> u32 {
+    input.0.clone().into_iter().filter(|&x| x == 0).count() as u32
+}
+
+pub fn part2(input: &(Vec<u32>, Vec<u32>)) -> u32 {
+    input.1.clone().into_iter().sum::<u32>()
+}
+
+pub fn do_rotations(input: &str, initial: i32) -> (Vec<u32>, Vec<u32>) {
+    let mut current = initial;
     input.lines().map(|line| {
+        let mut clicks: u32 = 0;
+        let mut new_current = current;
         let dir = line.as_bytes()[0] as char;
-        let mut count = line[1..].parse::<i32>().unwrap();
+        let count = line[1..].parse::<i32>().unwrap();
         if dir == 'L' {
-            count = 100 - (count % 100);
+            new_current = new_current - count;
         } else {
-            count = count % 100;
+            new_current = new_current + count;
         }
-        count as u32
-    }).collect()
-}
+        while new_current < 0 { new_current += 100; clicks += 1; }
+        // after being on the left, we may end up on 0, which has to be counted as a click
+        if new_current == 0 { clicks += 1; }
+        // compensate for starting at 0 and going Left, first click should be ignored
+        if current == 0 && dir == 'L' { clicks -= 1; }
+        while new_current >= 100 { new_current -= 100; clicks += 1; }
+        current = new_current;
 
-pub fn part1(input: &Vec<u32>) -> u32 {
-    input.iter()
-        .scan(50u32, |acc, x| {
-            *acc = ((*acc as u64 + *x as u64) % 100) as u32;
-            Some(*acc)
-        })
-        .filter(|&x| x == 0)
-        .count() as u32
-}
-
-pub fn part2(_input: &Vec<u32>) -> u32 {
-    0
-}
-
+        (current as u32, clicks as u32)
+    }).collect()}
--- tests/aoc2025/day01_test.rs
+++ tests/aoc2025/day01_test.rs
@@ -18,14 +18,31 @@ fn part1_test() {
     assert_eq!(part1(&input), 3);
 }
 
-// #[test]
-// fn part2_test() {
-//     let input = parse(EXAMPLE);
-//     assert_eq!(part2(&input), 0);
-// }
+#[test]
+fn part2_test() {
+    let input = parse(EXAMPLE);
+    assert_eq!(part2(&input), 6);
+}
 
 #[test]
-fn test_parsing_lines_into_100_offset() {
+fn test_parsing_data() {
     let seq = parse(EXAMPLE);
-    assert_eq!(seq, vec![32, 70, 48, 95, 60, 45, 99, 1, 14, 18]);
+    assert_eq!(seq.0, vec![82, 52, 0, 95, 55, 0, 99, 0, 14, 32]);
+    assert_eq!(seq.1, vec![1, 0, 1, 0, 1, 1, 0, 1, 0, 1]);
+}
+
+#[test]
+fn test_parsing_data2() {
+    assert_eq!(do_rotations("R1", 50), (vec![51], vec![0]));
+    assert_eq!(do_rotations("R50", 50), (vec![0], vec![1]));
+    assert_eq!(do_rotations("R51", 50), (vec![1], vec![1]));
+    assert_eq!(do_rotations("R150", 50), (vec![0], vec![2]));
+    assert_eq!(do_rotations("R151", 50), (vec![1], vec![2]));
+
+    assert_eq!(do_rotations("L1", 50), (vec![49], vec![0]));
+    assert_eq!(do_rotations("L50", 50), (vec![0], vec![1]));
+    assert_eq!(do_rotations("L51", 50), (vec![99], vec![1]));
+    assert_eq!(do_rotations("L150", 50), (vec![0], vec![2]));
+    assert_eq!(do_rotations("L151", 50), (vec![99], vec![2]));
+    assert_eq!(do_rotations("L766", 0), (vec![34], vec![7]));
 }
